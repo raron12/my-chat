89445eb788a9065459c97cf47b13ecff23331734
raron12 <raron997@gmail.com>
2020-05-04 19:26:18 +0200

    BUILD FILES

---
+++ build/precache-manifest.f9381d6799c2f1fd36d33cfa23b9f267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beab38b0e2ca449d105b4de0fd9141a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61305c7d3bd07408f89c",
+    "url": "./static/css/main.a7768bed.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61305c7d3bd07408f89c",
+    "url": "./static/js/main.41440065.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
